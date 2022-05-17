9e9b4d8fe6b19e6ecdf977a4c647c6c909e02e60
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-17 17:21:09 -0500

    🥅 [ERROR] Not Draggable component fuctionality

--- src/screens/animation102Screen/Animation102Screen.tsx
+++ src/screens/animation102Screen/Animation102Screen.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Animated, PanResponder, View } from 'react-native';
 
 import { styles } from './Animation102Screen.styles';
 
 const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: parseInt(pan.x.addListener(value => value.value)),
+          y: parseInt(pan.y.addListener(value => value.value)),
+        });
+      },
+      onPanResponderMove: (e, gestureState) =>
+        Animated.event(
+          [
+            null,
+            {
+              dx: pan.x,
+              dy: pan.y,
+            },
+          ],
+          { useNativeDriver: true },
+        )(e, gestureState),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
+    }),
+  ).current;
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}></View>
+      <Animated.View
+        style={[
+          styles.box,
+          {
+            transform: pan.getTranslateTransform(),
+          },
+        ]}
+        {...panResponder.panHandlers}></Animated.View>
     </View>
   );
 };
