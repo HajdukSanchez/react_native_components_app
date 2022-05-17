f1b516d0b080845ecec0d19d261c92a27b8690fc
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-17 13:00:58 -0500

    🚧 [ADD] Configurations and status bar

--- src/components/menuItem/MenuItem.styles.ts
+++ src/components/menuItem/MenuItem.styles.ts
@@ -1,7 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  information: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   text: {
+    marginLeft: 5,
     color: 'black',
     fontSize: 20,
   },
--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { styles } from './MenuItem.styles';
 import { MenuItemModel } from '../../models/MenuItem.model';
 
 const MenuItem = ({ name, component, icon }: MenuItemModel) => {
   return (
-    <View>
-      <Text style={styles.text}>
-        {name} - {icon}
-      </Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.4}>
+      <View style={styles.container}>
+        <View style={styles.information}>
+          <Icon name={icon} size={30} color={'grey'} />
+          <Text style={styles.text}>{name}</Text>
+        </View>
+        <View>
+          <Icon name="chevron-forward-outline" size={30} color={'grey'} />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
