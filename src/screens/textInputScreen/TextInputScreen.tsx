1c78a6cbf69cc243db45a6e5f35d45a1cb766b5e
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-18 12:40:51 -0500

    🚧 [ADD] Keyboard avoiding view

--- src/screens/textInputScreen/TextInputScreen.tsx
+++ src/screens/textInputScreen/TextInputScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import { styles } from './TextInputScreen.styles';
 import { HeaderTitle } from '../../components';
@@ -19,33 +19,39 @@ const TextInputScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <HeaderTitle title="Text Input" />
-      <TextInput
-        style={styles.textInput}
-        placeholder="Name"
-        placeholderTextColor={'grey'}
-        autoCorrect={false}
-        autoCapitalize="words"
-        onChangeText={value => handleInputChange(value, 'name')}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="Email"
-        placeholderTextColor={'grey'}
-        autoCorrect={false}
-        onChangeText={value => handleInputChange(value, 'email')}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="Phone "
-        placeholderTextColor={'grey'}
-        onChangeText={value => handleInputChange(value, 'phone')}
-        keyboardType="phone-pad"
-      />
-      <Text style={styles.text}>{JSON.stringify(form, null, 4)}</Text>
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <HeaderTitle title="Text Input" />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Name"
+              placeholderTextColor={'grey'}
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => handleInputChange(value, 'name')}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Email"
+              placeholderTextColor={'grey'}
+              autoCorrect={false}
+              onChangeText={value => handleInputChange(value, 'email')}
+              keyboardType="email-address"
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Phone "
+              placeholderTextColor={'grey'}
+              onChangeText={value => handleInputChange(value, 'phone')}
+              keyboardType="phone-pad"
+            />
+            <Text style={styles.text}>{JSON.stringify(form, null, 4)}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
