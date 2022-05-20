c79cde0dac6ca1517d965d4ab189da75da65a4cb
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-20 13:43:39 -0500

    🚧 [ADD] Theme based on SO

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,8 +1,9 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer, useRef } from 'react';
 
 import { themeReducer } from './themeReducer';
 import { darkTheme, lightTheme } from '../../theme';
 import { ThemeActionType, ThemeState } from '../../models/theme.model';
+import { Appearance, AppState, useColorScheme, Platform } from 'react-native';
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -17,7 +18,23 @@ interface ThemeProviderProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [theme, dispatch] = useReducer(themeReducer, lightTheme);
+  const colorScheme = useColorScheme();
+  const themeSO = useRef<ThemeState>(colorScheme === 'dark' ? darkTheme : lightTheme);
+  const [theme, dispatch] = useReducer(themeReducer, themeSO.current);
+
+  // This works in two platforms
+  useEffect(() => {
+    AppState.addEventListener('change', status => {
+      if (status === 'active') {
+        Appearance.getColorScheme() === 'light' ? setLightTheme() : setDarkTheme();
+      }
+    });
+  }, []);
+
+  // This only works on IOS
+  // useEffect(() => {
+  //   colorScheme === 'dark' ? (themeSO.current = darkTheme) : (themeSO.current = lightTheme);
+  // }, [colorScheme]);
 
   const setDarkTheme = () => {
     dispatch({ type: ThemeActionType.SET_DARK_THEME, payload: darkTheme });
